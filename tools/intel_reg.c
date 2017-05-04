ideak/intel-gpu-tools

commit 301ad44cdf1b868b1ab89096721da91fa8541fdc

lib: Open debugfs files for the given DRM device
When opening a DRM debugfs file, locate the right path based on the
given DRM device FD.

This is needed so, in setups with more than one DRM device, any
operations on debugfs files affect the expected DRM device.

v2: - rebased and fixed new API additions

v3: - updated chamelium test, which was missed previously
    - use the minor of the device for the debugfs path, not the major
    - have a proper exit handler for calling igt_hpd_storm_reset with the
      right device fd.

Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Reviewed-by: Robert Foss <robert.foss@collabora.com>
Acked-by: Chris Wilson <chris@chris-wilson.co.uk>

tools/intel_reg.c
@@ -32,6 +32,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "igt.h"
 #include "intel_io.h"
 #include "intel_chipset.h"
 
@@ -57,6 +58,7 @@ struct config {
 	struct pci_device *pci_dev;
 	char *mmiofile;
 	uint32_t devid;
+	int drm_fd;
 
 	/* read: number of registers to read */
 	uint32_t count;
@@ -409,7 +411,7 @@ static int intel_reg_read(struct config *config, int argc, char *argv[])
 	if (config->mmiofile)
 		intel_mmio_use_dump_file(config->mmiofile);
 	else
-		intel_register_access_init(config->pci_dev, 0);
+		intel_register_access_init(config->pci_dev, 0, config->drm_fd);
 
 	for (i = 1; i < argc; i++) {
 		struct reg reg;
@@ -439,7 +441,7 @@ static int intel_reg_write(struct config *config, int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	intel_register_access_init(config->pci_dev, 0);
+	intel_register_access_init(config->pci_dev, 0, config->drm_fd);
 
 	for (i = 1; i < argc; i += 2) {
 		struct reg reg;
@@ -477,7 +479,7 @@ static int intel_reg_dump(struct config *config, int argc, char *argv[])
 	if (config->mmiofile)
 		intel_mmio_use_dump_file(config->mmiofile);
 	else
-		intel_register_access_init(config->pci_dev, 0);
+		intel_register_access_init(config->pci_dev, 0, config->drm_fd);
 
 	for (i = 0; i < config->regcount; i++) {
 		reg = &config->regs[i];
@@ -877,6 +879,9 @@ int main(int argc, char *argv[])
 		config.devid = config.pci_dev->device_id;
 	}
 
+	/* Just to make sure we open the right debugfs files */
+	config.drm_fd = drm_open_driver_master(DRIVER_INTEL);
+
 	if (read_reg_spec(&config) < 0) {
 		return EXIT_FAILURE;
 	}
@@ -895,6 +900,7 @@ int main(int argc, char *argv[])
 
 	ret = command->function(&config, argc, argv);
 
+	close(config.drm_fd);
 	free(config.mmiofile);
 
 	return ret;

commit 862fa60cc9ead665fca4ddaa77210559b59d696b

intel_reg: Use the drm device fd as a hint only
We want intel_reg to work even when i915.ko isn't loaded, in which case
there is debugfs to provide /debug/.../i915_forcewake, but we will
survive without!

Fixes: 301ad44cdf1b ("lib: Open debugfs files for the given DRM device")
Reported-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

tools/intel_reg.c
@@ -880,7 +880,7 @@ int main(int argc, char *argv[])
 	}
 
 	/* Just to make sure we open the right debugfs files */
-	config.drm_fd = drm_open_driver_master(DRIVER_INTEL);
+	config.drm_fd = __drm_open_driver(DRIVER_INTEL);
 
 	if (read_reg_spec(&config) < 0) {
 		return EXIT_FAILURE;
@@ -900,7 +900,6 @@ int main(int argc, char *argv[])
 
 	ret = command->function(&config, argc, argv);
 
-	close(config.drm_fd);
 	free(config.mmiofile);
 
 	return ret;