eeeb304a00166757954ab0ebbbcaa405db8c0aeb
Eric Anholt <eric@anholt.net>
2017-06-07 16:51:21 -0700

    Make autogen.sh set the default format.subjectPrefix
    CONTRIBUTING requests that people do this, but it's a lot easier if we
    just set it up by default for them.
    
    v2: Update CONTRIBUTING about this happening (suggested by Petri).
    
    Signed-off-by: Eric Anholt <eric@anholt.net>
    Acked-by: Petri Latvala <petri.latvala@intel.com> (v1)

--- autogen.sh
+++ autogen.sh
@@ -16,6 +16,9 @@ fi
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
+git config --local --get format.subjectPrefix >/dev/null 2>&1 ||
+    git config --local format.subjectPrefix "PATCH i-g-t"
+
 if test -z "$NOCONFIGURE"; then
         $srcdir/configure "$@"
 fi
