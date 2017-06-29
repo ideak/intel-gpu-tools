2c299482351dc0253ce88a693ae70ecac81b8b31
Anusha Srivatsa <anusha.srivatsa@intel.com>
2017-06-28 23:16:14 -0700

    lib/cfl: Add PCI IDs to H SKU in CFl
    Follow the spec and add the ID for H SKU in
    CFL.
    
    v2: Update IDs following kernel commit:
    ccfd13215fd25a0e8c28221f3acc0dcaec11cd15 (Chris)
    
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Anusha Srivatsa <anusha.srivatsa@intel.com>
    Reviewed-by: Clint Taylor <clinton.a.taylor@intel.com>

--- lib/i915_pciids.h
+++ lib/i915_pciids.h
@@ -341,7 +341,13 @@
 	INTEL_VGA_DEVICE(0x3E92, info), /* SRV GT2 */ \
 	INTEL_VGA_DEVICE(0x3E96, info) /* SRV GT2 */
 
+
+#define INTEL_CFL_H_IDS(info) \
+	INTEL_VGA_DEVICE(0x3E9B, info), /* Halo GT2 */ \
+	INTEL_VGA_DEVICE(0x3E94, info) /* Halo GT2 */
+
 #define INTEL_CFL_IDS(info) \
-	INTEL_CFL_S_IDS(info)
+	INTEL_CFL_S_IDS(info), \
+	INTEL_CFL_H_IDS(info)
 
 #endif /* _I915_PCIIDS_H */
