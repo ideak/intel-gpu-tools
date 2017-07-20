ideak/intel-gpu-tools

commit ee31e0b539c63bb52137f0585605dad3a700a7c0

Introduce common frame dumping configuration and helpers
This introduces a common FrameDumpPath configuration field, as well as
helper functions in dedicated igt_frame for writing cairo surfaces
to png files.

Signed-off-by: Paul Kocialkowski <paul.kocialkowski@linux.intel.com>
Reviewed-by: Lyude <lyude@redhat.com>

lib/igt_core.c
@@ -235,6 +235,10 @@
  * An example configuration follows:
  *
  * |[<!-- language="plain" -->
+ *	# The common configuration secton follows.
+ *	[Common]
+ *	FrameDumpPath=/tmp # The path to dump frames that fail comparison checks
+ *
  *	# The following section is used for configuring the Device Under Test.
  *	# It is not mandatory and allows overriding default values.
  *	[DUT]
@@ -290,6 +294,7 @@ static struct {
 static pthread_mutex_t log_buffer_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 GKeyFile *igt_key_file;
+char *frame_dump_path;
 
 const char *igt_test_name(void)
 {
@@ -620,6 +625,13 @@ static int config_parse(void)
 
 	if (!igt_key_file)
 		return 0;
+
+	frame_dump_path = getenv("IGT_FRAME_DUMP_PATH");
+
+	if (!frame_dump_path)
+		frame_dump_path = g_key_file_get_string(igt_key_file, "Common",
+							"FrameDumpPath",
+							&error);
 
 	rc = g_key_file_get_integer(igt_key_file, "DUT", "SuspendResumeDelay",
 				    &error);

lib/Makefile.sources
@@ -83,6 +83,8 @@ lib_source_list =	 	\
 	uwildmat/uwildmat.c	\
 	igt_kmod.c		\
 	igt_kmod.h		\
+	igt_frame.c		\
+	igt_frame.h		\
 	$(NULL)
 
 .PHONY: version.h.tmp

lib/igt_core.h
@@ -50,7 +50,7 @@
 extern const char* __igt_test_description __attribute__((weak));
 extern bool __igt_plain_output;
 extern GKeyFile *igt_key_file;
-
+extern char *frame_dump_path;
 
 /**
  * IGT_TEST_DESCRIPTION:

lib/igt_frame.c
@@ -1,0 +1,137 @@
+/*
+ * Copyright © 2017 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *  Paul Kocialkowski <paul.kocialkowski@linux.intel.com>
+ */
+
+#include "config.h"
+
+#include <fcntl.h>
+#include <pixman.h>
+#include <cairo.h>
+
+#include "igt.h"
+
+/**
+ * SECTION:igt_frame
+ * @short_description: Library for frame-related tests
+ * @title: Frame
+ * @include: igt_frame.h
+ *
+ * This library contains helpers for frame-related tests. This includes common
+ * frame dumping as well as frame comparison helpers.
+ */
+
+/**
+ * igt_frame_dump_is_enabled:
+ *
+ * Get whether frame dumping is enabled.
+ *
+ * Returns: A boolean indicating whether frame dumping is enabled
+ */
+bool igt_frame_dump_is_enabled(void)
+{
+	return frame_dump_path != NULL;
+}
+
+static void igt_write_frame_to_png(cairo_surface_t *surface, int fd,
+				   const char *qualifier, const char *suffix)
+{
+	char path[PATH_MAX];
+	const char *test_name;
+	const char *subtest_name;
+	cairo_status_t status;
+	int index;
+
+	test_name = igt_test_name();
+	subtest_name = igt_subtest_name();
+
+	if (suffix)
+		snprintf(path, PATH_MAX, "%s/frame-%s-%s-%s-%s.png",
+			 frame_dump_path, test_name, subtest_name, qualifier,
+			 suffix);
+	else
+		snprintf(path, PATH_MAX, "%s/frame-%s-%s-%s.png",
+			 frame_dump_path, test_name, subtest_name, qualifier);
+
+	igt_debug("Dumping %s frame to %s...\n", qualifier, path);
+
+	status = cairo_surface_write_to_png(surface, path);
+
+	igt_assert_eq(status, CAIRO_STATUS_SUCCESS);
+
+	index = strlen(path);
+
+	if (fd >= 0 && index < (PATH_MAX - 1)) {
+		path[index++] = '\n';
+		path[index] = '\0';
+
+		write(fd, path, strlen(path));
+	}
+}
+
+/**
+ * igt_write_compared_frames_to_png:
+ * @reference: The reference cairo surface
+ * @capture: The captured cairo surface
+ * @reference_suffix: The suffix to give to the reference png file
+ * @capture_suffix: The suffix to give to the capture png file
+ *
+ * Write previously compared frames to png files.
+ */
+void igt_write_compared_frames_to_png(cairo_surface_t *reference,
+				      cairo_surface_t *capture,
+				      const char *reference_suffix,
+				      const char *capture_suffix)
+{
+	char *id;
+	const char *test_name;
+	const char *subtest_name;
+	char path[PATH_MAX];
+	int fd = -1;
+
+	if (!igt_frame_dump_is_enabled())
+		return;
+
+	id = getenv("IGT_FRAME_DUMP_ID");
+
+	test_name = igt_test_name();
+	subtest_name = igt_subtest_name();
+
+	if (id)
+		snprintf(path, PATH_MAX, "%s/frame-%s-%s-%s.txt",
+			 frame_dump_path, test_name, subtest_name, id);
+	else
+		snprintf(path, PATH_MAX, "%s/frame-%s-%s.txt",
+			 frame_dump_path, test_name, subtest_name);
+
+	fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	igt_assert(fd >= 0);
+
+	igt_debug("Writing dump report to %s...\n", path);
+
+	igt_write_frame_to_png(reference, fd, "reference", reference_suffix);
+	igt_write_frame_to_png(capture, fd, "capture", capture_suffix);
+
+	close(fd);
+}

lib/igt_frame.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 Intel Corporation
+ * Copyright © 2017 Intel Corporation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -19,36 +19,25 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
+ *
+ * Authors:
+ *  Paul Kocialkowski <paul.kocialkowski@linux.intel.com>
  */
 
-#ifndef IGT_H
-#define IGT_H
+#ifndef IGT_FRAME_H
+#define IGT_FRAME_H
 
-#include "drmtest.h"
-#include "i915_3d.h"
-#include "i915_pciids.h"
-#include "igt_aux.h"
-#include "igt_core.h"
-#include "igt_core.h"
-#include "igt_debugfs.h"
-#include "igt_draw.h"
-#include "igt_dummyload.h"
-#include "igt_fb.h"
-#include "igt_frame.h"
-#include "igt_gt.h"
-#include "igt_kms.h"
-#include "igt_pm.h"
-#include "igt_stats.h"
-#ifdef HAVE_CHAMELIUM
-#include "igt_chamelium.h"
+#ifdef HAVE_CONFIG_H
+#include "config.h"
 #endif
-#include "instdone.h"
-#include "intel_batchbuffer.h"
-#include "intel_chipset.h"
-#include "intel_io.h"
-#include "ioctl_wrappers.h"
-#include "media_fill.h"
-#include "media_spin.h"
-#include "rendercopy.h"
 
-#endif /* IGT_H */
+#include "igt.h"
+#include <stdbool.h>
+
+bool igt_frame_dump_is_enabled(void);
+void igt_write_compared_frames_to_png(cairo_surface_t *reference,
+				      cairo_surface_t *capture,
+				      const char *reference_suffix,
+				      const char *capture_suffix);
+
+#endif

commit a865fee5bbfa573823f758b85116e00934083f93

lib/igt_core: Handle glib errors correctly to avoid stderr spew
This adds the required error clean/free calls after calling
configuration parsing functions. In addition to properly handling memory,
this avoids glib spewing out error messages on stderr, which breaks the
whole CI with this message:

    GLib-WARNING **: GError set over the top of a previous GError or uninitialized memory.
    This indicates a bug in someone's code. You must ensure an error is NULL before it's set.
    The overwriting error message was: Key file does not have group ?DUT?

Fixes: ee31e0b5 ("Introduce common frame dumping configuration...")

Cc: Paul Kocialkowski <paul.kocialkowski@linux.intel.com>
Cc: Lyude <lyude@redhat.com>
Cc: Martin Peres <martin.peres@linux.intel.com>
Acked-by: Arkadiusz Hiler <arkadiusz.hiler@intel.com>
Signed-off-by: Paul Kocialkowski <paul.kocialkowski@linux.intel.com>

lib/igt_core.c
@@ -637,10 +637,16 @@ static int config_parse(void)
 							"FrameDumpPath",
 							&error);
 
+	g_clear_error(&error);
+
 	rc = g_key_file_get_integer(igt_key_file, "DUT", "SuspendResumeDelay",
 				    &error);
-	if (error && error->code == G_KEY_FILE_ERROR_INVALID_VALUE)
+	if (error && error->code == G_KEY_FILE_ERROR_INVALID_VALUE) {
+		g_error_free(error);
 		return -2;
+	}
+
+	g_clear_error(&error);
 
 	if (rc != 0)
 		igt_set_autoresume_delay(rc);
@@ -809,12 +815,15 @@ static int common_init(int *argc, char **argv,
 	ret = g_key_file_load_from_file(igt_key_file, key_file_loc,
 					G_KEY_FILE_NONE, &error);
 	if (error && error->code == G_KEY_FILE_ERROR) {
+		g_error_free(error);
 		g_key_file_free(igt_key_file);
 		igt_key_file = NULL;
 		ret = -2;
 
 		goto out;
 	}
+
+	g_clear_error(&error);
 
 	ret = config_parse();
 #endif