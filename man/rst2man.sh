895236c6ead83ed537d762394c9930b1e82d8dde
Daniel Vetter <daniel.vetter@ffwll.ch>
2017-09-05 14:36:21 +0200

    meson: add manpage support
    It seems like meson doesn't want you to string together targets
    like make does, but wants it all in one step. So another little
    shell script it is.
    
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Arkadiusz Hiler <arkadiusz.hiler@intel.com>
    Acked-by: Petri Latvala <petri.latvala@intel.com>
    Acked-by: Daniel Stone <daniels@collabora.com>
    Acked-by: Radoslaw Szwichtenberg <radoslaw.szwichtenberg@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

---
+++ man/rst2man.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+input=$1
+output=$2
+
+out_dir=$(dirname ${output})
+in_file=$(basename ${input})
+
+# rst2man doesn't handle multiple source directories well, and since defs.rst is
+# generated we first need to move it all into the build dir
+cp $input $out_dir
+
+rst2man $out_dir/$in_file ${output%.gz}
+
+rm -f ${output}
+gzip ${output%.gz}
