ideak/intel-gpu-tools

commit 20d6e0f41b3b70a89946e5271bc4ffa7695be1b0

tools/intel_gpu_frequency: remove use of getsubopt
getsubopt is not available in android. The "get" option
doesn't really need sub-options, just display all the
current frequency settings (as per discussion with
Ben Widawsky)

Ben v2: Remove the -geff example in the header
Fixed another typo for the --set while there (found by Dave Gordon)

Signed-off-by: Tim Gore <tim.gore@intel.com>
Reviewed-by: Ben Widawsky <ben@bwidawsk.net>

tools/intel_gpu_frequency.c
@@ -22,16 +22,13 @@
  *
  * Example:
  * Get all frequencies:
- * intel_gpu_frequency --get=cur,min,max,eff
+ * intel_gpu_frequency --get
  *
  * Same as above:
  * intel_gpu_frequency -g
  *
- * Get the efficient frequency:
- * intel_gpu_frequency -geff
- *
  * Lock the GPU frequency to 300MHz:
- * intel_gpu_frequency --set min=300
+ * intel_gpu_frequency --set 300
  *
  * Set the maximum frequency to 900MHz:
  * intel_gpu_frequency --custom max=900
@@ -152,7 +149,7 @@ usage(const char *prog)
 	printf("Usage: %s [-e] [--min | --max] [-g (min|max|efficient)] [-s frequency_mhz]\n\n", prog);
 	printf("Options: \n");
 	printf("  -e		Lock frequency to the most efficient frequency\n");
-	printf("  -g, --get=    Get the frequency (optional arg: \"cur\"|\"min\"|\"max\"|\"eff\")\n");
+	printf("  -g, --get     Get all the frequency settings\n");
 	printf("  -s, --set     Lock frequency to an absolute value (MHz)\n");
 	printf("  -c, --custom  Set a min, or max frequency \"min=X | max=Y\"\n");
 	printf("  -m  --max     Lock frequency to max frequency\n");
@@ -184,13 +181,6 @@ parse(int argc, char *argv[], bool *act_upon, size_t act_upon_n, int *new_freq)
 	int c, tmp;
 	bool write = false;
 
-	char *token[] = {
-		(char *)info[CUR].name,
-		(char *)info[MIN].name,
-		(char *)"eff",
-		(char *)info[MAX].name
-	};
-
 	/* No args means -g" */
 	if (argc == 1) {
 		for (c = 0; c < act_upon_n; c++)
@@ -200,7 +190,7 @@ parse(int argc, char *argv[], bool *act_upon, size_t act_upon_n, int *new_freq)
 	while (1) {
 		int option_index = 0;
 		static struct option long_options[] = {
-			{ "get", optional_argument, NULL, 'g' },
+			{ "get", no_argument, NULL, 'g' },
 			{ "set", required_argument, NULL, 's' },
 			{ "custom", required_argument, NULL, 'c'},
 			{ "min", no_argument, NULL, 'i' },
@@ -211,7 +201,7 @@ parse(int argc, char *argv[], bool *act_upon, size_t act_upon_n, int *new_freq)
 			{ NULL, 0, NULL, 0}
 		};
 
-		c = getopt_long(argc, argv, "eg::s:c:midh", long_options, &option_index);
+		c = getopt_long(argc, argv, "egs:c:midh", long_options, &option_index);
 		if (c == -1)
 			break;
 
@@ -219,19 +209,7 @@ parse(int argc, char *argv[], bool *act_upon, size_t act_upon_n, int *new_freq)
 		case 'g':
 			if (write == true)
 				fprintf(stderr, "Read and write operations not support simultaneously.\n");
-
-			if (optarg) {
-				char *value, *subopts = optarg;
-				int x;
-				while (*subopts != '\0') {
-					x = getsubopt(&subopts, token, &value);
-					if (x == -1) {
-						fprintf(stderr, "Unrecognized option (%s)\n", value);
-						break;
-					} else
-						act_upon[x] = true;
-				}
-			} else {
+			{
 				int i;
 				for (i = 0; i < act_upon_n; i++)
 					act_upon[i] = true;

man/intel_gpu_frequency.man
@@ -3,7 +3,7 @@
 intel_gpu_frequency: \- manual page for intel_gpu_frequency
 .SH SYNOPSIS
 .B intel_gpu_frequency
-[\fI\,-e\/\fR] [\fI\,--min | --max\/\fR] [\fI\,-g (min|max|efficient)\/\fR] [\fI\,-s frequency_mhz\/\fR]
+[\fI\,-e\/\fR] [\fI\,--min | --max\/\fR] [\fI\,-g\/\fR] [\fI\,-s frequency_mhz\/\fR]
 .SH DESCRIPTION
 \&A program to manipulate Intel GPU frequencies. Intel GPUs
 will automatically throttle the frequencies based on system demands, up when
@@ -19,8 +19,8 @@ safe bet.
 \fB\-e\fR
 Lock frequency to the most efficient frequency
 .TP
-\fB\-g\fR, \fB\-\-get=\fR
-Get the frequency comma separated list of ("cur"|"min"|"max"|"eff")
+\fB\-g\fR, \fB\-\-get\fR
+Get all the current frequency settings
 .TP
 \fB\-s\fR, \fB\-\-set\fR
 Lock frequency to an absolute value (MHz)

commit 95bfb2902473b9f4e644c3eb831fdf110d87ed4f

tools/intel_gpu_frequency: fix usage and examples
The cited commit removed --get suboptions but failed to update the usage
and examples. Remove the duplicated examples from the comments in the
interest of if not single but at least fewer points of truth.

Fixes: 20d6e0f41b3b ("tools/intel_gpu_frequency: remove use of getsubopt")
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=99492
Reported-by: Andreas Reis <andreas.reis@gmail.com>
Reviewed-by: Radoslaw Szwichtenberg <radoslaw.szwichtenberg@intel.com>
Signed-off-by: Jani Nikula <jani.nikula@intel.com>

tools/intel_gpu_frequency.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 Intel Corporation
+ * Copyright © 2015,2018 Intel Corporation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -19,31 +19,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
- *
- * Example:
- * Get all frequencies:
- * intel_gpu_frequency --get
- *
- * Same as above:
- * intel_gpu_frequency -g
- *
- * Lock the GPU frequency to 300MHz:
- * intel_gpu_frequency --set 300
- *
- * Set the maximum frequency to 900MHz:
- * intel_gpu_frequency --custom max=900
- *
- * Lock the GPU frequency to its maximum frequency:
- * intel_gpu_frequency --max
- *
- * Lock the GPU frequency to its most efficient frequency:
- * intel_gpu_frequency -e
- *
- * Lock The GPU frequency to its minimum frequency:
- * intel_gpu_frequency --min
- *
- * Reset the GPU to hardware defaults
- * intel_gpu_frequency -d
  */
 
 #include <assert.h>
@@ -145,7 +120,7 @@ static void __attribute__((noreturn))
 usage(const char *prog)
 {
 	printf("%s A program to manipulate Intel GPU frequencies.\n\n", prog);
-	printf("Usage: %s [-e] [--min | --max] [-g (min|max|efficient)] [-s frequency_mhz]\n\n", prog);
+	printf("Usage: %s [-e] [--min | --max] [--get] [--set frequency_mhz]\n\n", prog);
 	printf("Options: \n");
 	printf("  -e		Lock frequency to the most efficient frequency\n");
 	printf("  -g, --get     Get all the frequency settings\n");
@@ -158,9 +133,9 @@ usage(const char *prog)
 	printf("  -v  --version Version\n");
 	printf("\n");
 	printf("Examples:\n");
-	printf("   intel_gpu_frequency -gmin,cur\tGet the current and minimum frequency\n");
-	printf("   intel_gpu_frequency -s 400\tLock frequency to 400Mhz\n");
-	printf("   intel_gpu_frequency -c max=750\tSet the max frequency to 750MHz\n");
+	printf("   intel_gpu_frequency --get\t\tGet the current and minimum frequency\n");
+	printf("   intel_gpu_frequency --set 400\tLock frequency to 400Mhz\n");
+	printf("   intel_gpu_frequency --custom max=750\tSet the max frequency to 750MHz\n");
 	printf("\n");
 	printf("Report bugs to <bugs.freedesktop.org>\n");
 	exit(EXIT_FAILURE);
@@ -170,7 +145,7 @@ static void
 version(const char *prog)
 {
 	printf("%s: %s\n", prog, VERSION);
-	printf("Copyright © 2015 Intel Corporation\n");
+	printf("Copyright © 2015,2018 Intel Corporation\n");
 }
 
 /* Returns read or write operation */