c3035d8cb11459f186f712d9c7ebb6734fc99831
Chris Wilson <chris@chris-wilson.co.uk>
2018-07-09 11:27:44 +0100

    igt/gem_render_copy: Check for GEM before running
    gem_render_copy requires a working GPU so check first.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- tests/gem_render_copy.c
+++ tests/gem_render_copy.c
@@ -529,6 +529,7 @@ int main(int argc, char **argv)
 	igt_fixture {
 		data.drm_fd = drm_open_driver_render(DRIVER_INTEL);
 		data.devid = intel_get_drm_devid(data.drm_fd);
+		igt_require_gem(data.drm_fd);
 
 		data.bufmgr = drm_intel_bufmgr_gem_init(data.drm_fd, 4096);
 		igt_assert(data.bufmgr);
