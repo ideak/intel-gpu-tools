ideak/intel-gpu-tools

commit 20087bf22698612a526353f022bc232e2b0dcdcc

lib: Use a bsearch to find the module name
Even with a small number of known drivers (6), a bsearch will take at
most 3 steps, whereas the linear search will take 3 steps on average. In
the future with more known drivers, the logN bsearch will be even more
advantageous.

Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
Cc: Katarzyna Dec <katarzyna.dec@intel.com>
Reviewed-by: Katarzyna Dec <katarzyna.dec@intel.com>

lib/drmtest.c
@@ -222,9 +222,15 @@ static int open_device(const char *name, unsigned int chipset)
 	if (__get_drm_device_name(fd, dev_name, sizeof(dev_name) - 1) == -1)
 		goto err;
 
-	for (const struct module *m = modules; m->module; m++) {
-		if (strcmp(m->module, dev_name) == 0) {
-			chip = m->bit;
+	for (int start = 0, end = ARRAY_SIZE(modules) - 1; start < end; ){
+		int mid = start + (end - start) / 2;
+		int ret = strcmp(modules[mid].module, dev_name);
+		if (ret < 0) {
+			end = mid;
+		} else if (ret > 0) {
+			start = mid + 1;
+		} else {
+			chip = modules[mid].bit;
 			break;
 		}
 	}

commit 125eee6e981eac0a004aeb4f327439a132ceac5c

lib: Fix strcmp() direction for bsearch.
I fluked out as vgem was the initial mid value, hiding the worst of the
errors as i915 matched with DRIVER_ANY.

Fixes: 20087bf22698 ("lib: Use a bsearch to find the module name")
Reported-by: Petri Latvala <petri.latvala@intel.com>
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
Cc: Petri Latvala <petri.latvala@intel.com>
Reviewed-by: Petri Latvala <petri.latvala@intel.com>

lib/drmtest.c
@@ -226,9 +226,9 @@ static int open_device(const char *name, unsigned int chipset)
 		int mid = start + (end - start) / 2;
 		int ret = strcmp(modules[mid].module, dev_name);
 		if (ret < 0) {
-			end = mid;
-		} else if (ret > 0) {
 			start = mid + 1;
+		} else if (ret > 0) {
+			end = mid;
 		} else {
 			chip = modules[mid].bit;
 			break;