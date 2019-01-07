6c9a70f0ed51832a762e009c490a082f5d19e25d
José Roberto de Souza <jose.souza@intel.com>
2019-01-07 09:41:23 -0800

    tests/fbcon_fbt: Allow fbcon to bind when running this test
    9c4114ec5d87 (lib: Always unbind the fbcon around igt) broke
    fbcon_fbt test as fbcon is not allowed to bind when executing any IGT
    test, so lets allow it again just for this test.
    
    Cc: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Cc: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Reviewed-by: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
    Signed-off-by: José Roberto de Souza <jose.souza@intel.com>

--- tests/kms_fbcon_fbt.c
+++ tests/kms_fbcon_fbt.c
@@ -26,6 +26,7 @@
 
 #include "igt.h"
 #include "igt_psr.h"
+#include "igt_sysfs.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -279,6 +280,12 @@ static void setup_environment(void)
 	drm_fd = drm_open_driver_master(DRIVER_INTEL);
 	igt_require(drm_fd >= 0);
 	igt_assert(close(drm_fd) == 0);
+
+	/*
+	 * igt_main()->igt_subtest_init_parse_opts()->common_init() disables the
+	 * fbcon bind, so to test it is necessary enable it again
+	 */
+	bind_fbcon(true);
 }
 
 static void teardown_environment(void)
