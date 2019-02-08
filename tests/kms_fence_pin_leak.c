9ee4da8d60bb3d1c6a94ae8d69a86bf819c3831f
Chris Wilson <chris@chris-wilson.co.uk>
2019-02-08 14:38:00 +0000

    kms_fence_pin_leak: Ask for the GPU before use
    Check that the GPU even exists before submitting a batch.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=109589
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>

--- tests/kms_fence_pin_leak.c
+++ tests/kms_fence_pin_leak.c
@@ -201,6 +201,7 @@ igt_simple_main
 	igt_skip_on_simulation();
 
 	data.drm_fd = drm_open_driver_master(DRIVER_INTEL);
+	igt_require_gem(data.drm_fd);
 
 	data.devid = intel_get_drm_devid(data.drm_fd);
 
