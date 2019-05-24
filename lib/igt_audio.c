ideak/intel-gpu-tools

commit 42576d7102551b814929cb35bee64272ff0d44ca

lib/igt_audio: add support for S24_LE and S32_LE signal generation
This adds two new helpers to generate S24_LE and S32_LE signals.

Signed-off-by: Simon Ser <simon.ser@intel.com>
Reviewed-by: Martin Peres <martin.peres@linux.intel.com>

lib/igt_audio.c
@@ -251,6 +251,8 @@ static void audio_sanity_check(double *samples, size_t samples_len)
  * Fill the requested number of samples to the target buffer with the audio
  * signal data (in interleaved double format), at the requested sampling rate
  * and number of channels.
+ *
+ * Each sample is normalized (ie. between 0 and 1).
  */
 void audio_signal_fill(struct audio_signal *signal, double *buffer,
 		       size_t samples)
@@ -312,6 +314,36 @@ void audio_signal_fill_s16_le(struct audio_signal *signal, int16_t *buffer,
 
 	for (i = 0; i < signal->channels * samples; ++i)
 		buffer[i] = INT16_MAX * tmp[i];
+
+	free(tmp);
+}
+
+void audio_signal_fill_s24_le(struct audio_signal *signal, int32_t *buffer,
+			      size_t samples)
+{
+	double *tmp;
+	size_t i;
+
+	tmp = malloc(sizeof(double) * signal->channels * samples);
+	audio_signal_fill(signal, tmp, samples);
+
+	for (i = 0; i < signal->channels * samples; ++i)
+		buffer[i] = 0xFFFFFF * tmp[i];
+
+	free(tmp);
+}
+
+void audio_signal_fill_s32_le(struct audio_signal *signal, int32_t *buffer,
+			      size_t samples)
+{
+	double *tmp;
+	size_t i;
+
+	tmp = malloc(sizeof(double) * signal->channels * samples);
+	audio_signal_fill(signal, tmp, samples);
+
+	for (i = 0; i < signal->channels * samples; ++i)
+		buffer[i] = UINT32_MAX * tmp[i];
 
 	free(tmp);
 }

lib/igt_audio.h
@@ -44,6 +44,10 @@ void audio_signal_fill(struct audio_signal *signal, double *buffer,
 		       size_t buffer_len);
 void audio_signal_fill_s16_le(struct audio_signal *signal, int16_t *buffer,
 			      size_t buffer_len);
+void audio_signal_fill_s24_le(struct audio_signal *signal, int32_t *buffer,
+			      size_t buffer_len);
+void audio_signal_fill_s32_le(struct audio_signal *signal, int32_t *buffer,
+			      size_t buffer_len);
 bool audio_signal_detect(struct audio_signal *signal, int sampling_rate,
 			 int channel, const double *samples, size_t samples_len);
 size_t audio_extract_channel_s32_le(double *dst, size_t dst_cap,

commit 5771480d7648dcd66d00e5a97b502027437fcc71

lib/igt_audio: fix amplitude for S24_LE and S32_LE
We need to use signed integers (hence "S" in the format name).

The reason this has gone unnoticed is that we were generating signals with half
of the amplitude they were supposed to have. As a result, using UINT32_MAX
(which is almost INT32_MAX * 2) resulted in the correct amplitude. In other
words, these two bugs were cancelling each other's effects.

Signed-off-by: Simon Ser <simon.ser@intel.com>
Fixes: 42576d710255 ("lib/igt_audio: add support for S24_LE and S32_LE signal generation")

lib/igt_audio.c
@@ -328,7 +328,7 @@ void audio_signal_fill_s24_le(struct audio_signal *signal, int32_t *buffer,
 	audio_signal_fill(signal, tmp, samples);
 
 	for (i = 0; i < signal->channels * samples; ++i)
-		buffer[i] = 0xFFFFFF * tmp[i];
+		buffer[i] = 0x7FFFFF * tmp[i];
 
 	free(tmp);
 }
@@ -343,7 +343,7 @@ void audio_signal_fill_s32_le(struct audio_signal *signal, int32_t *buffer,
 	audio_signal_fill(signal, tmp, samples);
 
 	for (i = 0; i < signal->channels * samples; ++i)
-		buffer[i] = UINT32_MAX * tmp[i];
+		buffer[i] = INT32_MAX * tmp[i];
 
 	free(tmp);
 }