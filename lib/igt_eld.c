ideak/intel-gpu-tools

commit 3374cd0b048f9c277b2815bf80502f9f89680176

lib/igt_eld: introduce eld_is_supported
We've seen cases in which /proc/asound doesn't exist (e.g. with the new SOF
framework). We've also seen cases in which no soundcard is exposed by ALSA (see
bugzilla link). Last, some audio drivers din't support ELDs (non-Intel
drivers). In all of these cases, skipping the tests depending on ELD support
makes more sense and makes it clearer what happens.

v2: also check that the driver supports ELDs entries in procfs

Signed-off-by: Simon Ser <simon.ser@intel.com>
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=102370
Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>

lib/igt_eld.c
@@ -26,6 +26,8 @@
 #include "config.h"
 
 #include <dirent.h>
+#include <errno.h>
+#include <glob.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -261,4 +263,20 @@ bool eld_has_igt(void)
 {
 	struct eld_entry eld;
 	return eld_get_igt(&eld);
+}
+
+/** eld_is_supported: check whether the ALSA procfs is enabled, audio cards
+ * are found and ELDs are supported */
+bool eld_is_supported(void)
+{
+	glob_t glob_buf = {0};
+	bool has_elds;
+
+	igt_assert_f(glob("/proc/asound/card*/" ELD_PREFIX "*",
+			  GLOB_NOSORT, NULL, &glob_buf) == 0,
+		     "glob failed\n");
+	has_elds = glob_buf.gl_pathc > 0;
+	globfree(&glob_buf);
+
+	return has_elds;
 }

lib/igt_eld.h
@@ -49,6 +49,7 @@ struct eld_entry {
 	struct eld_sad sads[ELD_SADS_CAP];
 };
 
+bool eld_is_supported(void);
 bool eld_get_igt(struct eld_entry *eld);
 bool eld_has_igt(void);
 

tests/kms_chamelium.c
@@ -1968,6 +1968,8 @@ test_display_audio_edid(data_t *data, struct chamelium_port *port,
 	struct eld_entry eld;
 	struct eld_sad *sad;
 
+	igt_require(eld_is_supported());
+
 	reset_state(data, port);
 
 	output = prepare_output(data, port, edid);

tests/kms_hdmi_inject.c
@@ -142,6 +142,8 @@ hdmi_inject_audio(int drm_fd, drmModeConnector *connector)
 	struct igt_fb fb;
 	struct kmstest_connector_config config;
 
+	igt_require(eld_is_supported());
+
 	edid = igt_kms_get_hdmi_audio_edid();
 	kmstest_force_edid(drm_fd, connector, edid);
 

commit 8a611399226cfa248f91f80d263b84b7f13cabee

lib/igt_eld: fix eld_is_supported failing if not supported
When glob(3) doesn't find any results, it returns GLOB_NOMATCH. I've been
confused by GLOB_NOCHECK's description and thought it would return 0.

Instead of failing on the assert, return that ELDs aren't supported in case
there's no match.

While at it, also include glob's return value in the assert message.

Signed-off-by: Simon Ser <simon.ser@intel.com>
Fixes: 3374cd0b048f ("lib/igt_eld: introduce eld_is_supported")
Cc: Petri Latvala <petri.latvala@intel.com>
Reviewed-by: Petri Latvala <petri.latvala@intel.com>

lib/igt_eld.c
@@ -270,11 +270,14 @@ bool eld_has_igt(void)
 bool eld_is_supported(void)
 {
 	glob_t glob_buf = {0};
+	int ret;
 	bool has_elds;
 
-	igt_assert_f(glob("/proc/asound/card*/" ELD_PREFIX "*",
-			  GLOB_NOSORT, NULL, &glob_buf) == 0,
-		     "glob failed\n");
+	ret = glob("/proc/asound/card*/" ELD_PREFIX "*",
+		   GLOB_NOSORT, NULL, &glob_buf);
+	if (ret == GLOB_NOMATCH)
+		return false;
+	igt_assert_f(ret == 0, "glob failed: %d\n", ret);
 	has_elds = glob_buf.gl_pathc > 0;
 	globfree(&glob_buf);
 