ideak/intel-gpu-tools

commit 8b90eefce9b47db1ad05857797efa5213778dbec

lib/igt_fb: Use render copy/blit on platforms w/o HW detiling
On platforms without HW detiling use render copy or blitting to convert
a framebuffer to a cairo surface.

Replaces [1] which used a slow device_coherent map, instead of the
faster engine copy in this version.

[1] https://patchwork.freedesktop.org/patch/349742/?series=72308&rev=1

Cc: Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>
Signed-off-by: Imre Deak <imre.deak@intel.com>
Reviewed-by: Ashutosh Dixit <ashutosh.dixit@intel.com>

lib/igt_fb.c
@@ -1975,6 +1975,9 @@ static bool fast_blit_ok(const struct igt_fb *fb)
 
 static bool blitter_ok(const struct igt_fb *fb)
 {
+	if (is_ccs_modifier(fb->modifier))
+		return false;
+
 	for (int i = 0; i < fb->num_planes; i++) {
 		int width = fb->plane_width[i];
 
@@ -2003,16 +2006,22 @@ static bool blitter_ok(const struct igt_fb *fb)
 
 static bool use_enginecopy(const struct igt_fb *fb)
 {
-	return is_ccs_modifier(fb->modifier) ||
-		(fb->modifier == I915_FORMAT_MOD_Yf_TILED &&
-		 !blitter_ok(fb));
+	if (blitter_ok(fb))
+		return false;
+
+	return fb->modifier == I915_FORMAT_MOD_Yf_TILED ||
+	       is_ccs_modifier(fb->modifier) ||
+	       !gem_has_mappable_ggtt(fb->fd);
 }
 
 static bool use_blitter(const struct igt_fb *fb)
 {
-	return (fb->modifier == I915_FORMAT_MOD_Y_TILED ||
-		fb->modifier == I915_FORMAT_MOD_Yf_TILED) &&
-		blitter_ok(fb);
+	if (!blitter_ok(fb))
+		return false;
+
+	return fb->modifier == I915_FORMAT_MOD_Y_TILED ||
+	       fb->modifier == I915_FORMAT_MOD_Yf_TILED ||
+	       !gem_has_mappable_ggtt(fb->fd);
 }
 
 static void init_buf_ccs(struct igt_buf *buf, int ccs_idx,

commit 4ab7c2120a89d77769871db186e739ff1342ee41

lib/igt_fb: Avoid hitting assertion on non-intel HW
Recently a call to intel_get_drm_devid was added in a code path that
gets run on all hardware.

Restore the previous behavior by bailing out before we reach the assert.

Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
Fixes: 8b90eefce9b4 ("lib/igt_fb: Use render copy/blit on platforms w/o HW detiling")
Reviewed-by: Petri Latvala <petri.latvala@intel.com>

lib/igt_fb.c
@@ -1992,6 +1992,9 @@ static bool fast_blit_ok(const struct igt_fb *fb)
 
 static bool blitter_ok(const struct igt_fb *fb)
 {
+	if (!is_i915_device(fb->fd))
+		return false;
+
 	if (is_ccs_modifier(fb->modifier))
 		return false;
 