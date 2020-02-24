ideak/intel-gpu-tools

commit 3fd64d9fb8561d28c5a6d4943c13eb681ce1e7db

lib/i915-perf: Add support for loading perf configurations
Loads programming of various HW bits into i915.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Acked-by: Chris Wilson <chris@chris-wilson.co.uk>
Acked-by: Petri Latvala <petri.latvala@intel.com>

lib/i915/perf.c
@@ -422,3 +422,90 @@ intel_perf_add_metric_set(struct intel_perf *perf,
 {
 	igt_list_add_tail(&metric_set->link, &perf->metric_sets);
 }
+
+static void
+load_metric_set_config(struct intel_perf_metric_set *metric_set, int drm_fd)
+{
+	struct drm_i915_perf_oa_config config;
+	uint64_t config_id = 0;
+
+	memset(&config, 0, sizeof(config));
+
+	memcpy(config.uuid, metric_set->hw_config_guid, sizeof(config.uuid));
+
+	config.n_mux_regs = metric_set->n_mux_regs;
+	config.mux_regs_ptr = (uintptr_t) metric_set->mux_regs;
+
+	config.n_boolean_regs = metric_set->n_b_counter_regs;
+	config.boolean_regs_ptr = (uintptr_t) metric_set->b_counter_regs;
+
+	config.n_flex_regs = metric_set->n_flex_regs;
+	config.flex_regs_ptr = (uintptr_t) metric_set->flex_regs;
+
+	while (ioctl(drm_fd, DRM_IOCTL_I915_PERF_ADD_CONFIG, &config) < 0 &&
+	       (errno == EAGAIN || errno == EINTR));
+
+	metric_set->perf_oa_metrics_set = config_id;
+}
+
+void
+intel_perf_load_perf_configs(struct intel_perf *perf, int drm_fd)
+{
+	int sysfs_dir_fd = open_master_sysfs_dir(drm_fd);
+	struct dirent *entry;
+	int metrics_dir_fd;
+	DIR *metrics_dir;
+	struct intel_perf_metric_set *metric_set;
+
+	if (sysfs_dir_fd < 0)
+		return;
+
+	metrics_dir_fd = openat(sysfs_dir_fd, "metrics", O_DIRECTORY);
+	close(sysfs_dir_fd);
+	if (metrics_dir_fd < -1)
+		return;
+
+	metrics_dir = fdopendir(metrics_dir_fd);
+	if (!metrics_dir) {
+		close(metrics_dir_fd);
+		return;
+	}
+
+	while ((entry = readdir(metrics_dir))) {
+		bool metric_id_read;
+		uint64_t metric_id;
+		char path[256 + 4];
+		int id_fd;
+
+		if (entry->d_type != DT_DIR)
+			continue;
+
+		snprintf(path, sizeof(path), "%s/id", entry->d_name);
+
+		id_fd = openat(metrics_dir_fd, path, O_RDONLY);
+		if (id_fd < 0)
+			continue;
+
+		metric_id_read = read_fd_uint64(id_fd, &metric_id);
+		close(id_fd);
+
+		if (!metric_id_read)
+			continue;
+
+		igt_list_for_each_entry(metric_set, &perf->metric_sets, link) {
+			if (!strcmp(metric_set->hw_config_guid, entry->d_name)) {
+				metric_set->perf_oa_metrics_set = metric_id;
+				break;
+			}
+		}
+	}
+
+	closedir(metrics_dir);
+
+	igt_list_for_each_entry(metric_set, &perf->metric_sets, link) {
+		if (metric_set->perf_oa_metrics_set)
+			continue;
+
+		load_metric_set_config(metric_set, drm_fd);
+	}
+}

lib/i915/perf.h
@@ -220,6 +220,8 @@ void intel_perf_add_logical_counter(struct intel_perf *perf,
 void intel_perf_add_metric_set(struct intel_perf *perf,
 			       struct intel_perf_metric_set *metric_set);
 
+void intel_perf_load_perf_configs(struct intel_perf *perf, int drm_fd);
+
 #ifdef __cplusplus
 };
 #endif

commit 9bc102c0d4e0057a919ef2af75fd6beb8436c01a

lib/i915/perf: fix loading configurations
The configuration ID created is returned not passed by argument.

This would lead the verify first test in the perf tests to fail.

v2: Take the opportunity to add a local ioctl helper

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 3fd64d9fb8 ("lib/i915-perf: Add support for loading perf configurations")
Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>

lib/i915/perf.c
@@ -39,6 +39,17 @@
 #include "perf.h"
 #include "i915_perf_metrics.h"
 
+static int
+perf_ioctl(int fd, unsigned long request, void *arg)
+{
+	int ret;
+
+	do {
+		ret = ioctl(fd, request, arg);
+	} while (ret == -1 && (errno == EINTR || errno == EAGAIN));
+	return ret;
+}
+
 static struct intel_perf_logical_counter_group *
 intel_perf_logical_counter_group_new(struct intel_perf *perf,
 				     struct intel_perf_logical_counter_group *parent,
@@ -237,8 +248,7 @@ getparam(int drm_fd, uint32_t param)
         gp.param = param;
         gp.value = &val;
 
-	while (ioctl(drm_fd, DRM_IOCTL_I915_GETPARAM, &gp) < 0 &&
-	       (errno == EAGAIN || errno == EINTR));
+	perf_ioctl(drm_fd, DRM_IOCTL_I915_GETPARAM, &gp);
 
         return val;
 }
@@ -281,11 +291,8 @@ query_items(int drm_fd, struct drm_i915_query_item *items, uint32_t n_items)
 		.num_items = n_items,
 		.items_ptr = (uintptr_t) items,
 	};
-	int ret;
 
-	while ((ret = ioctl(drm_fd, DRM_IOCTL_I915_QUERY, &q)) < 0 &&
-	       (errno == EAGAIN || errno == EINTR));
-	return ret;
+	return perf_ioctl(drm_fd, DRM_IOCTL_I915_QUERY, &q);
 }
 
 static struct drm_i915_query_topology_info *
@@ -427,7 +434,7 @@ static void
 load_metric_set_config(struct intel_perf_metric_set *metric_set, int drm_fd)
 {
 	struct drm_i915_perf_oa_config config;
-	uint64_t config_id = 0;
+	int ret;
 
 	memset(&config, 0, sizeof(config));
 
@@ -442,10 +449,9 @@ load_metric_set_config(struct intel_perf_metric_set *metric_set, int drm_fd)
 	config.n_flex_regs = metric_set->n_flex_regs;
 	config.flex_regs_ptr = (uintptr_t) metric_set->flex_regs;
 
-	while (ioctl(drm_fd, DRM_IOCTL_I915_PERF_ADD_CONFIG, &config) < 0 &&
-	       (errno == EAGAIN || errno == EINTR));
-
-	metric_set->perf_oa_metrics_set = config_id;
+	ret = perf_ioctl(drm_fd, DRM_IOCTL_I915_PERF_ADD_CONFIG, &config);
+	if (ret >= 0)
+		metric_set->perf_oa_metrics_set = ret;
 }
 
 void