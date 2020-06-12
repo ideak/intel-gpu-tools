ideak/intel-gpu-tools

commit 0421ebe98f0bc903ac9991d03142bf35c14f16c2

lib/i915: Increase range for randomised location of dummyload
Suggest a wider range of possible relocations to reduce the chance of
colliding with a framebuffer.

Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
Acked-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>

lib/igt_dummyload.c
@@ -99,7 +99,9 @@ emit_recursive_batch(igt_spin_t *spin,
 	 * are not allowed in the first 256KiB, for fear of negative relocations
 	 * that wrap.
 	 */
-	addr = (random() % 1024 + 1024) << 12;
+	addr = gem_aperture_size(fd) / 2;
+	addr += random() % addr / 2;
+	addr &= -4096;
 
 	nengine = 0;
 	if (opts->engine == ALL_ENGINES) {

commit f66c71e29de50bae880bc81ceb0517d4e3e2dfd8

lib/i915: Fix GTT offset for 64b
Large GTT do not fit into the 32b local, use a 64b and limit to [2G-3G]!

Fixes: 0421ebe98f0b ("lib/i915: Increase range for randomised location of dummyload")
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
Reviewed-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>

lib/igt_dummyload.c
@@ -82,7 +82,7 @@ emit_recursive_batch(igt_spin_t *spin,
 	unsigned int nengine;
 	int fence_fd = -1;
 	uint32_t *cs, *batch;
-	uint32_t addr;
+	uint64_t addr;
 	int i;
 
 	/*
@@ -100,6 +100,8 @@ emit_recursive_batch(igt_spin_t *spin,
 	 * that wrap.
 	 */
 	addr = gem_aperture_size(fd) / 2;
+	if (addr >> 31)
+		addr = 1 << 31;
 	addr += random() % addr / 2;
 	addr &= -4096;
 