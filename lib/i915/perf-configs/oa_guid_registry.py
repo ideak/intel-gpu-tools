ideak/intel-gpu-tools

commit 53f8f541caae201ceb24160b1690ea94c60af9a7

lib: Add i915_perf library
This library contains the database of performance metrics for each
supported generation as well as its associated programming.

It also allow for the user to get human readable data out of HW
generated data.

v2: Switch a few python scripts to python3

v3: Fix some python2-ism
    s/lkf/ehl/
    Autotools support

v4: set soversion on i915_perf.so

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Acked-by: Chris Wilson <chris@chris-wilson.co.uk>
Acked-by: Petri Latvala <petri.latvala@intel.com>

lib/i915/perf-configs/oa_guid_registry.py
@@ -1,0 +1,73 @@
+
+import copy
+import hashlib
+
+import xml.etree.ElementTree as et
+
+
+class Registry:
+
+    # Tries to avoid fragility from et.tostring() by normalizing into CSV string first
+    @staticmethod
+    def hw_config_hash(metric_set):
+        """Hashes the given metric set's HW register configs.
+
+        Args:
+            metric_set -- is an ElementTree element for a 'set'
+
+        Note this doesn't accept an MDAPI based metric set description
+        """
+
+        registers_str = ""
+        for config in metric_set.findall(".//register_config"):
+            if config.get('id') == None:
+                config_id = '0'
+            else:
+                config_id = config.get('id')
+            if config.get('priority') == None:
+                config_priority = '0'
+            else:
+                config_priority = config.get('priority')
+            if config.get('availability') == None:
+                config_availability = ""
+            else:
+                config_availability = config.get('availability')
+            for reg in config.findall("register"):
+                addr = int(reg.get('address'), 16)
+                value = int(reg.get('value'), 16)
+                registers_str = registers_str + config_id + ',' + config_priority + ',' + config_availability + ',' + str(addr) + ',' + str(value) + '\n'
+
+        return hashlib.md5(registers_str.encode('utf-8')).hexdigest()
+
+
+    @staticmethod
+    def mdapi_hw_config_hash(mdapi_metric_set):
+        """Hashes the HW register configuration of a metric set from VPG's MDAPI XML files.
+
+        Args:
+            mdapi_metric_set -- is an ElementTree element for a 'MetricSet'
+
+        Note: being a simplistic hash of all RegConfigStart element contents
+        this will change for minor comment changes in VPG's files. Without
+        any promisies of stability within these files then it can't help to
+        err on the side of caution here, so we know when to investigate
+        changes that might affect our useages.
+        """
+
+        config = et.Element('config')
+        for registers in mdapi_metric_set.findall(".//RegConfigStart"):
+            config.append(copy.deepcopy(registers))
+        registers_str = et.tostring(config)
+
+        return hashlib.md5(registers_str).hexdigest()
+
+
+    @staticmethod
+    def chipset_derive_hash(chipset, hash):
+        """Derive a HW config hash for a given chipset.
+
+        This helps us avoiding collisions with identical config across
+        different Gen or GT.
+        """
+
+        return "%s-%s" % (chipset, hash)

configure.ac
@@ -26,6 +26,8 @@ AC_INIT([igt-gpu-tools],
 	[https://bugs.freedesktop.org/enter_bug.cgi?product=DRI&component=IGT],
 	[igt-gpu-tools])
 
+AC_SUBST([i915_perf_version], [1.0.0], [libi915_perf.so version])
+
 AC_CONFIG_SRCDIR([Makefile.am])
 AC_CONFIG_HEADERS([config.h])
 AC_CONFIG_MACRO_DIR([m4])
@@ -304,10 +306,12 @@ AC_CONFIG_FILES([
 		 Makefile
 		 benchmarks/Makefile
 		 lib/Makefile
+		 lib/i915-perf.pc
 		 scripts/Makefile
 		 tests/Makefile
 		 tests/intel-ci/Makefile
 		 tools/Makefile
+		 tools/i915-perf/Makefile
 		 tools/null_state_gen/Makefile
 		 tools/registers/Makefile
 		 overlay/Makefile

lib/Makefile.am
@@ -1,6 +1,25 @@
 IGT_LIB_PATH := $(builddir)
 GPU_TOOLS_PATH := $(top_srcdir)
 
+i915_perf_hardware =		\
+	hsw			\
+	bdw chv			\
+	sklgt2 sklgt3 sklgt4	\
+	kblgt2 kblgt3		\
+	cflgt2 cflgt3		\
+	bxt glk			\
+	cnl			\
+	icl ehl			\
+	tgl
+
+i915_perf_xml_files_ = $(addprefix i915/perf-configs/oa-,$(i915_perf_hardware))
+i915_perf_xml_files = $(addsuffix .xml,$(i915_perf_xml_files_))
+
+$(builddir)/i915/i915_perf_metrics.h: i915/perf-configs/perf-codegen.py $(i915_perf_xml_files)
+	$(PYTHON) i915/perf-configs/perf-codegen.py --code $(builddir)/i915/i915_perf_metrics.c --header $(builddir)/i915/i915_perf_metrics.h $(i915_perf_xml_files)
+
+$(builddir)/i915/i915_perf_metrics.c: $(builddir)/i915/i915_perf_metrics.h
+
 include Makefile.sources
 
 libintel_tools_la_SOURCES = $(lib_source_list)
@@ -8,6 +27,17 @@ libintel_tools_la_SOURCES = $(lib_source_list)
 libigt_perf_la_SOURCES = \
 	igt_perf.c	 \
 	igt_perf.h
+
+libi915_perf_la_SOURCES = $(i915_perf_sources)
+libi915_perf_HEADERS =	\
+	igt_list.h	\
+	i915/perf.h
+libi915_perfdir = $(includedir)/i915-perf
+
+pkgconfigdir = $(libdir)/pkgconfig
+pkgconfig_DATA = i915-perf.pc
+
+lib_LTLIBRARIES = libi915_perf.la
 
 noinst_LTLIBRARIES = libintel_tools.la libigt_perf.la
 noinst_HEADERS = check-ndebug.h

lib/Makefile.sources
@@ -142,6 +142,13 @@ lib_source_list =	 	\
 	veboxcopy_gen12.c	\
 	$(NULL)
 
+i915_perf_sources =			\
+	igt_list.c			\
+	i915/perf.c	 		\
+	i915/perf.h			\
+	i915/i915_perf_metrics.c	\
+	i915/i915_perf_metrics.h
+
 .PHONY: version.h.tmp
 
 # leaving a space here to work around automake's conditionals
@@ -172,6 +179,8 @@ $(IGT_LIB_PATH)/version.h: $(IGT_LIB_PATH)/version.h.tmp
 		rm $(IGT_LIB_PATH)/version.h.tmp ; \
 	fi
 
-BUILT_SOURCES = $(IGT_LIB_PATH)/version.h
+BUILT_SOURCES =				\
+	$(IGT_LIB_PATH)/version.h	\
+	i915/i915_perf_metrics.c	\
+	i915/i915_perf_metrics.h
 CLEANFILES = $(IGT_LIB_PATH)/version.h $(IGT_LIB_PATH)/version.h.tmp
-

lib/i915-perf.pc.in
@@ -1,0 +1,10 @@
+prefix=@prefix@
+exec_prefix=@exec_prefix@
+libdir=@libdir@
+includedir=@includedir@
+
+Name: i915-perf
+Description: i915 perf library
+Version: @i915_perf_version@
+Libs: -L${libdir} -li915_perf
+Cflags: -I${includedir}/i915-perf

lib/i915/perf-configs/codegen.py
@@ -1,0 +1,33 @@
+class Codegen:
+
+    _file = None
+    _indent = 0
+
+    endl="\n"
+    use_tabs = False
+
+    def __init__(self, filename = None):
+        if filename != None:
+            self._file = open(filename, 'w')
+
+    def __call__(self, *args):
+        if self._file:
+            code = ' '.join(map(str, args))
+            for line in code.splitlines():
+                indent = ''.rjust(self._indent)
+
+                if self.use_tabs:
+                    indent = indent.replace("        ", "\t")
+
+                text = indent + line
+                self._file.write(text.rstrip() + self.endl)
+
+    #without indenting or new lines
+    def frag(self, *args):
+        code = ' '.join(map(str, args))
+        self._file.write(code)
+
+    def indent(self, n):
+        self._indent = self._indent + n
+    def outdent(self, n):
+        self._indent = self._indent - n

commit 7c4a8b3113d2fb9f23c1a7f9236a3e5036b82aa2

lib/i915/perf: fixup converter script
This script was previous run in python2, but because IGT is python3
only there are differences in what it generates. In particular the
attribute order differs in the tostring() of the ElementTree
function...

Reorder them alphabetically manually.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 53f8f541caae ("lib: Add i915_perf library")
Acked-by: Ashutosh Dixit <ashutosh.dixit@intel.com>
Reviewed-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>

lib/i915/perf-configs/oa_guid_registry.py
@@ -54,9 +54,19 @@ class Registry:
         changes that might affect our useages.
         """
 
+        def reorder_attributes(root):
+            for el in root.iter():
+                attrib = el.attrib
+                if len(attrib) > 1:
+                    # adjust attribute order, e.g. by sorting
+                    attribs = sorted(attrib.items())
+                    attrib.clear()
+                    attrib.update(attribs)
+
         config = et.Element('config')
         for registers in mdapi_metric_set.findall(".//RegConfigStart"):
             config.append(copy.deepcopy(registers))
+        reorder_attributes(config)
         registers_str = et.tostring(config)
 
         return hashlib.md5(registers_str).hexdigest()