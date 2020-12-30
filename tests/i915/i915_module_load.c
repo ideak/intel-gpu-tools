9a1986d1fc258b52bbf3c30cc01b5bb88f82ae05
Chris Wilson <chris@chris-wilson.co.uk>
2020-12-30 18:31:40 +0000

    i915/module_load: Wean sanitycheck off legacy ring selection ABI
    Use the engine map to utilise all engines during the sanitycheck
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Acked-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>

--- tests/i915/i915_module_load.c
+++ tests/i915/i915_module_load.c
@@ -20,7 +20,6 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-#include "i915/gem_ring.h"
 #include "igt.h"
 #include "igt_debugfs.h"
 #include "igt_aux.h"
@@ -37,70 +36,6 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 
-#define ENGINE_MASK  (I915_EXEC_RING_MASK | I915_EXEC_BSD_MASK)
-
-static void store_dword(int fd, unsigned ring)
-{
-	const unsigned int gen = intel_gen(intel_get_drm_devid(fd));
-	struct drm_i915_gem_exec_object2 obj[2];
-	struct drm_i915_gem_relocation_entry reloc;
-	struct drm_i915_gem_execbuffer2 execbuf;
-	uint32_t batch[16];
-	int i;
-
-	if (!gem_has_ring(fd, ring))
-		return;
-
-	if (!gem_can_store_dword(fd, ring))
-		return;
-
-	intel_detect_and_clear_missed_interrupts(fd);
-	memset(&execbuf, 0, sizeof(execbuf));
-	execbuf.buffers_ptr = (uintptr_t)obj;
-	execbuf.buffer_count = 2;
-	execbuf.flags = ring;
-	if (gen < 6)
-		execbuf.flags |= I915_EXEC_SECURE;
-
-	memset(obj, 0, sizeof(obj));
-	obj[0].handle = gem_create(fd, 4096);
-	obj[1].handle = gem_create(fd, 4096);
-
-	memset(&reloc, 0, sizeof(reloc));
-	reloc.target_handle = obj[0].handle;
-	reloc.presumed_offset = 0;
-	reloc.offset = sizeof(uint32_t);
-	reloc.delta = 0;
-	reloc.read_domains = I915_GEM_DOMAIN_INSTRUCTION;
-	reloc.write_domain = I915_GEM_DOMAIN_INSTRUCTION;
-	obj[1].relocs_ptr = (uintptr_t)&reloc;
-	obj[1].relocation_count = 1;
-
-	i = 0;
-	batch[i] = MI_STORE_DWORD_IMM | (gen < 6 ? 1 << 22 : 0);
-	if (gen >= 8) {
-		batch[++i] = 0;
-		batch[++i] = 0;
-	} else if (gen >= 4) {
-		batch[++i] = 0;
-		batch[++i] = 0;
-		reloc.offset += sizeof(uint32_t);
-	} else {
-		batch[i]--;
-		batch[++i] = 0;
-	}
-	batch[++i] = 0xc0ffee;
-	batch[++i] = MI_BATCH_BUFFER_END;
-	gem_write(fd, obj[1].handle, 0, batch, sizeof(batch));
-	gem_execbuf(fd, &execbuf);
-	gem_close(fd, obj[1].handle);
-
-	gem_read(fd, obj[0].handle, 0, batch, sizeof(batch));
-	gem_close(fd, obj[0].handle);
-	igt_assert_eq(*batch, 0xc0ffee);
-	igt_assert_eq(intel_detect_and_clear_missed_interrupts(fd), 0);
-}
-
 static void store_all(int fd)
 {
 	const unsigned int gen = intel_gen(intel_get_drm_devid(fd));
@@ -109,20 +44,16 @@ static void store_all(int fd)
 	struct drm_i915_gem_exec_object2 obj[2];
 	struct drm_i915_gem_relocation_entry reloc[2 * ARRAY_SIZE(engines)];
 	struct drm_i915_gem_execbuffer2 execbuf;
+	const struct intel_execution_engine2 *e;
 	uint32_t batch[16];
 	uint64_t offset;
 	unsigned nengine;
 	int value;
 	int i, j;
 
-	if (!gem_can_store_dword(fd, 0))
-		return;
-
 	memset(&execbuf, 0, sizeof(execbuf));
 	execbuf.buffers_ptr = (uintptr_t)obj;
 	execbuf.buffer_count = 2;
-	if (gen < 6)
-		execbuf.flags |= I915_EXEC_SECURE;
 
 	memset(reloc, 0, sizeof(reloc));
 	memset(obj, 0, sizeof(obj));
@@ -149,14 +80,16 @@ static void store_all(int fd)
 
 	nengine = 0;
 	intel_detect_and_clear_missed_interrupts(fd);
-	for_each_ring(e, fd) {
-		if (!gem_can_store_dword(fd, eb_ring(e)))
+	__for_each_physical_engine(fd, e) {
+		if (!gem_class_can_store_dword(fd, e->class))
 			continue;
 
 		igt_assert(2 * (nengine + 1) * sizeof(batch) <= 4096);
 
-		execbuf.flags &= ~ENGINE_MASK;
-		execbuf.flags |= eb_ring(e);
+		engines[nengine] = e->flags;
+		if (gen < 6)
+			engines[nengine] |= I915_EXEC_SECURE;
+		execbuf.flags = engines[nengine];
 
 		j = 2*nengine;
 		reloc[j].target_handle = obj[0].handle;
@@ -188,7 +121,7 @@ static void store_all(int fd)
 		execbuf.batch_start_offset = j*sizeof(batch);
 		gem_execbuf(fd, &execbuf);
 
-		engines[nengine++] = eb_ring(e);
+		nengine++;
 	}
 	gem_sync(fd, obj[1].handle);
 
@@ -198,14 +131,12 @@ static void store_all(int fd)
 		memcpy(permuted, engines, nengine*sizeof(engines[0]));
 		igt_permute_array(permuted, nengine, igt_exchange_int);
 		for (j = 0; j < nengine; j++) {
-			execbuf.flags &= ~ENGINE_MASK;
-			execbuf.flags |= permuted[j];
+			execbuf.flags = permuted[j];
 			gem_execbuf(fd, &execbuf);
 		}
 		obj[1].relocs_ptr = (uintptr_t)&reloc[2*i+1];
 		execbuf.batch_start_offset = (2*i+1)*sizeof(batch);
-		execbuf.flags &= ~ENGINE_MASK;
-		execbuf.flags |= engines[i];
+		execbuf.flags = engines[i];
 		gem_execbuf(fd, &execbuf);
 	}
 	gem_close(fd, obj[1].handle);
@@ -255,42 +186,24 @@ inject_fault(const char *module_name, const char *opt, int fault)
 	return fault;
 }
 
-static void
-gem_sanitycheck(void)
+static void gem_sanitycheck(void)
 {
-	int err = 0;
-	int fd;
 	struct drm_i915_gem_caching args = {};
+	int i915 = __drm_open_driver(DRIVER_INTEL);
+	int err;
 
-
-	fd = __drm_open_driver(DRIVER_INTEL);
-	igt_set_timeout(1, "Module reload timeout!");
-
-	if (ioctl(fd, DRM_IOCTL_I915_GEM_SET_CACHING, &args) < 0)
+	err = 0;
+	if (ioctl(i915, DRM_IOCTL_I915_GEM_SET_CACHING, &args))
 		err = -errno;
+	if (err == -ENOENT) {
+		igt_fork_hang_detector(i915);
+		store_all(i915);
+		igt_stop_hang_detector();
+	}
+	errno = 0;
 
-	igt_set_timeout(0, NULL);
-	close(fd);
-
+	close(i915);
 	igt_assert_eq(err, -ENOENT);
-}
-
-static void
-gem_exec_store(void)
-{
-	int fd;
-	const struct intel_execution_ring *e;
-
-	fd = __drm_open_driver(DRIVER_INTEL);
-	igt_fork_hang_detector(fd);
-
-	for (e = intel_execution_rings; e->name; e++)
-		store_dword(fd, eb_ring(e));
-
-	store_all(fd);
-
-	igt_stop_hang_detector();
-	close(fd);
 }
 
 static void
@@ -334,7 +247,6 @@ igt_main
 		igt_assert_eq(load_error, 0);
 
 		gem_sanitycheck();
-		gem_exec_store();
 
 		/* only default modparams, can leave module loaded */
 	}
