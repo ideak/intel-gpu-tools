ideak/intel-gpu-tools

commit 801309d0c245cbf8115c03978d0819f484b477c0

lib/i915: Add ADL-P plane offset restriction for CCS framebuffers
All DPT FB plane offsets must be 2MB-aligned. The kernel takes care of
aligning the offset for non-CCS framebuffers.  Make sure that the
allocated CCS FB layout meets the alignment requirement.

Signed-off-by: Imre Deak <imre.deak@intel.com>
Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>

lib/igt_fb.c
@@ -788,11 +788,23 @@ static uint64_t calc_plane_size(struct igt_fb *fb, int plane)
 		return (uint64_t) fb->strides[plane] *
 			ALIGN(fb->plane_height[plane], tile_height);
 	} else if (is_gen12_ccs_plane(fb, plane)) {
+		uint64_t size;
+
 		/* The AUX CCS surface must be page aligned */
-		return (uint64_t)fb->strides[plane] *
+		size = (uint64_t)fb->strides[plane] *
 			ALIGN(fb->plane_height[plane], 64);
+
+		/*
+		 * On ADL_P CCS color planes must be 2MB aligned, until remapping
+		 * support is added for CCS FBs.
+		 */
+		if (IS_ALDERLAKE_P(intel_get_drm_devid(fb->fd)))
+			size = ALIGN(size, 2 * 1024 * 1024);
+
+		return size;
 	} else {
 		unsigned int tile_width, tile_height;
+		uint64_t size;
 
 		igt_get_fb_tile_size(fb->fd, fb->modifier, fb->plane_bpp[plane],
 				     &tile_width, &tile_height);
@@ -801,8 +813,18 @@ static uint64_t calc_plane_size(struct igt_fb *fb, int plane)
 		if (is_gen12_ccs_modifier(fb->modifier))
 			tile_height *= 4;
 
-		return (uint64_t) fb->strides[plane] *
+		size = (uint64_t)fb->strides[plane] *
 			ALIGN(fb->plane_height[plane], tile_height);
+
+		/*
+		 * On ADL_P CCS color planes must be 2MB aligned, until remapping
+		 * support is added for CCS FBs.
+		 */
+		if (IS_ALDERLAKE_P(intel_get_drm_devid(fb->fd)) &&
+		    is_ccs_modifier(fb->modifier))
+			size = ALIGN(size, 2 * 1024 * 1024);
+
+		return size;
 	}
 }
 

commit 61ba2ed489540e6a8a649be38abb075b3ab4d28a

lib/igt_fb: Add checks for intel device
Certain tests (eg: kms_plane_multiple) are failing on amdgpu due to
an assert in intel_get_drm_devid which checks for an i915 device.

Add check for intel device before calling intel_get_drm_devid.

Fixes: 801309d0c245 ("lib/i915: Add ADL-P plane offset restriction for CCS framebuffers")

Signed-off-by: Anson Jacob <Anson.Jacob@amd.com>
Cc: Imre Deak <imre.deak@intel.com>
Reviewed-by: Petri Latvala <petri.latvala@intel.com>

lib/igt_fb.c
@@ -760,7 +760,8 @@ static uint32_t calc_plane_stride(struct igt_fb *fb, int plane)
 			stride = ALIGN(min_stride, tile_width * 4);
 
 			/* TODO: add support to kernel to POT align CCS format strides */
-			if (IS_ALDERLAKE_P(intel_get_drm_devid(fb->fd)))
+			if (is_i915_device(fb->fd) &&
+			    IS_ALDERLAKE_P(intel_get_drm_devid(fb->fd)))
 				stride = roundup_power_of_two(max(stride, tile_width * 8));
 		} else {
 			stride = ALIGN(min_stride, tile_width);
@@ -836,7 +837,8 @@ static uint64_t calc_plane_size(struct igt_fb *fb, int plane)
 		 * On ADL_P CCS color planes must be 2MB aligned, until remapping
 		 * support is added for CCS FBs.
 		 */
-		if (IS_ALDERLAKE_P(intel_get_drm_devid(fb->fd)) &&
+		if (is_i915_device(fb->fd) &&
+		    IS_ALDERLAKE_P(intel_get_drm_devid(fb->fd)) &&
 		    is_ccs_modifier(fb->modifier))
 			size = ALIGN(size, 2 * 1024 * 1024);
 