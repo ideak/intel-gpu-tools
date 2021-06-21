e5f1f29523c43fad5efd458fe93a5f66ef312523
Boris Brezillon <boris.brezillon@collabora.com>
2021-06-21 14:42:59 +0200

    lib/panfrost: Get rid of igt_panfrost_trivial_job()
    We no longer use fragment jobs to test job submission, let's get rid
    of igt_panfrost_trivial_job(). If we ever need to issue fragment
    jobs again, we'll probably have to pull the pan_pack() infra from
    mesa and make the helper deal with Bifrost specificities.
    
    v3:
    * Remove igt_panfrost_trivial_job() prototype
    
    v2:
    * Add R-b
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Reviewed-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- lib/igt_panfrost.h
+++ lib/igt_panfrost.h
@@ -50,7 +50,6 @@ void igt_panfrost_free_bo(int fd, struct panfrost_bo *bo);
 struct mali_job_descriptor_header *
 igt_panfrost_job_loop_get_job_header(struct panfrost_submit *submit,
                                      unsigned job_idx);
-struct panfrost_submit *igt_panfrost_trivial_job(int fd, bool do_crash, int width, int height, uint32_t color);
 struct panfrost_submit *igt_panfrost_job_loop(int fd);
 struct panfrost_submit *igt_panfrost_null_job(int fd);
 void igt_panfrost_free_job(int fd, struct panfrost_submit *submit);
