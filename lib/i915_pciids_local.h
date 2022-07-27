1c3f7a3496b1b6cb2d1d12acac1a19136ae4058e
Matt Roper <matthew.d.roper@intel.com>
2022-07-26 21:34:52 -0700

    lib: sync i915_pciids.h with kernel
    This synchronizes with kernel commit
    
    7835303982d1 ("drm/i915/mtl: Add MeteorLake PCI IDs")
    to bring in DG2/ATS-M and initial MTL PCI IDs.
    
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Acked-by: Wayne Boyer <wayne.boyer@intel.com>

--- lib/i915_pciids_local.h
+++ lib/i915_pciids_local.h
@@ -7,25 +7,4 @@
 
 #include "i915_pciids.h"
 
-/* DG2 */
-#define INTEL_DG2_IDS(info) \
-	INTEL_VGA_DEVICE(0x56A0, info), \
-	INTEL_VGA_DEVICE(0x56A1, info), \
-	INTEL_VGA_DEVICE(0x56A2, info), \
-	INTEL_VGA_DEVICE(0x56A3, info), \
-	INTEL_VGA_DEVICE(0x56A4, info), \
-	INTEL_VGA_DEVICE(0x56A5, info), \
-	INTEL_VGA_DEVICE(0x56A6, info)
-
-/* ATS-M */
-#define INTEL_ATS_M150_IDS(info) \
-	INTEL_VGA_DEVICE(0x56C0, info)
-
-#define INTEL_ATS_M75_IDS(info) \
-	INTEL_VGA_DEVICE(0x56C1, info)
-
-#define INTEL_ATS_M_IDS(info) \
-	INTEL_ATS_M150_IDS(info), \
-	INTEL_ATS_M75_IDS(info)
-
 #endif /* _I915_PCIIDS_LOCAL_H */
