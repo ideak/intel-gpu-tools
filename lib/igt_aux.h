ideak/intel-gpu-tools

commit 2983efbdb1ab6475b7a8d09b1eb5e9a9cd3bc5b8

lib/igt_aux: Let's use the official mem_sleep names
Mixing the names with the state names can cause confusion.
Let's stick with the official names.

Cc: Riana Tauro <riana.tauro@intel.com>
Cc: Anshuman Gupta <anshuman.gupta@intel.com>
Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
Reviewed-by: Anshuman Gupta <anshuman.gupta@intel.com>

lib/igt_aux.h
@@ -196,9 +196,9 @@ enum igt_suspend_test {
  */
 enum igt_mem_sleep {
 	MEM_SLEEP_NONE,
-	MEM_SLEEP_FREEZE,
-	MEM_SLEEP_STANDBY,
-	MEM_SLEEP_MEM,
+	MEM_SLEEP_S2IDLE,
+	MEM_SLEEP_SHALLOW,
+	MEM_SLEEP_DEEP,
 
 	/*<private>*/
 	MEM_SLEEP_NUM,

lib/igt_aux.c
@@ -712,9 +712,9 @@ static const char *suspend_test_name[] = {
 };
 
 static const char *mem_sleep_name[] = {
-	[MEM_SLEEP_FREEZE] = "s2idle",
-	[MEM_SLEEP_STANDBY] = "shallow",
-	[MEM_SLEEP_MEM] = "deep"
+	[MEM_SLEEP_S2IDLE] = "s2idle",
+	[MEM_SLEEP_SHALLOW] = "shallow",
+	[MEM_SLEEP_DEEP] = "deep"
 };
 
 static enum igt_suspend_test get_suspend_test(int power_dir)
@@ -993,7 +993,7 @@ int igt_get_memsleep_state(void)
 		return MEM_SLEEP_NONE;
 	}
 
-	for (mem_sleep = MEM_SLEEP_FREEZE; mem_sleep < MEM_SLEEP_NUM; mem_sleep++) {
+	for (mem_sleep = MEM_SLEEP_S2IDLE; mem_sleep < MEM_SLEEP_NUM; mem_sleep++) {
 		if (strcmp(mem_sleep_name[mem_sleep], mem_sleep_state) == 0)
 			break;
 	}

tests/i915/i915_suspend.c
@@ -209,7 +209,7 @@ test_suspend_without_i915(int state)
 	igt_kmsg(KMSG_INFO "Unloading i915\n");
 	igt_assert_eq(igt_i915_driver_unload(),0);
 
-	igt_skip_on_f(igt_get_memsleep_state() == MEM_SLEEP_FREEZE && state > SUSPEND_STATE_FREEZE,
+	igt_skip_on_f(igt_get_memsleep_state() == MEM_SLEEP_S2IDLE && state > SUSPEND_STATE_FREEZE,
 		      "Platform default mem_sleep state is s2idle\n");
 	igt_system_suspend_autoresume(state, SUSPEND_TEST_NONE);
 

commit 62500db378f8e32b423b7854a838f10c2de964ac

lib/igt_aux: Fix igt_mem_sleep doc
I recently had updated the names to use the official names, but
forgot to update the doc.

CC: Ashutosh Dixit <ashutosh.dixit@intel.com>
Fixes: 2983efbdb1a ("lib/igt_aux: Let's use the official mem_sleep names")
Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
Reviewed-by: Ashutosh Dixit <ashutosh.dixit@intel.com>

lib/igt_aux.h
@@ -197,9 +197,9 @@ enum igt_suspend_test {
 /**
  * igt_mem_sleep:
  * @MEM_SLEEP_NONE: no support
- * @MEM_SLEEP_FREEZE: suspend-to-idle target state, aka S0ix or freeze,
- * @MEM_SLEEP_STANDBY: standby target state, aka S1
- * @MEM_SLEEP_MEM: suspend-to-mem target state aka S3
+ * @MEM_SLEEP_S2IDLE: suspend-to-idle target state, aka S0ix or freeze,
+ * @MEM_SLEEP_SHALLOW: standby target state, aka S1
+ * @MEM_SLEEP_DEEP: suspend-to-mem target state aka S3
  */
 enum igt_mem_sleep {
 	MEM_SLEEP_NONE,