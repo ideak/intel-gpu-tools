29676fdc2478cfef5cc15ae8c75677b08662636f
Bhanuprakash Modem <bhanuprakash.modem@intel.com>
2023-02-23 20:39:43 +0530

    tests/kms_dp_tiled_display: Add support for Bigjoiner
    This patch will add a check to Skip the subtest if a selected pipe/output
    combo won't support Bigjoiner or 8K mode.
    
    Example:
    * Pipe-D wont support a mode > 5K
    * To use 8K mode on a pipe then consecutive pipe must be available & free.
    
    V2: - Use updated helper name
    
    Signed-off-by: Bhanuprakash Modem <bhanuprakash.modem@intel.com>
    Reviewed-by: Karthik B S <karthik.b.s@intel.com>

--- tests/kms_dp_tiled_display.c
+++ tests/kms_dp_tiled_display.c
@@ -272,6 +272,7 @@ static void setup_mode(data_t *data)
 		igt_require(found);
 		igt_output_override_mode(output, mode);
 	}
+	igt_require(i915_pipe_output_combo_valid(data->display));
 	igt_display_commit_atomic(data->display, DRM_MODE_ATOMIC_ALLOW_MODESET,
 				  NULL);
 }
