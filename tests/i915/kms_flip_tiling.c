30d155dc737a7290f03f1f8202053e1e3621c06d
Bhanuprakash Modem <bhanuprakash.modem@intel.com>
2023-02-23 20:40:11 +0530

    tests/i915/kms_flip_tiling: Add support for Bigjoiner
    This patch will add a check to Skip the subtest if a selected pipe/output
    combo won't support Bigjoiner or 8K mode.
    
    Example:
    * Pipe-D wont support a mode > 5K
    * To use 8K mode on a pipe then consecutive pipe must be available & free.
    
    V2: - Use updated helper name
    V3: - Remove unwanted prints
    
    Signed-off-by: Bhanuprakash Modem <bhanuprakash.modem@intel.com>
    Reviewed-by: Jeevan B <jeevan.b@intel.com>

--- tests/i915/kms_flip_tiling.c
+++ tests/i915/kms_flip_tiling.c
@@ -195,8 +195,12 @@ igt_main
 		for_each_pipe_with_valid_output(&data.display, pipe, output) {
 			igt_plane_t *plane;
 
+			igt_display_reset(&data.display);
 			pipe_crc_free(&data);
+
 			igt_output_set_pipe(output, pipe);
+			if (!i915_pipe_output_combo_valid(&data.display))
+				continue;
 
 			plane = igt_output_get_plane_type(output, DRM_PLANE_TYPE_PRIMARY);
 
