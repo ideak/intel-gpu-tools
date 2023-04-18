15cbe39d181c4b282b1d84fbf12ba94c37a03aa4
Petri Latvala <petri.latvala@intel.com>
2023-04-18 21:06:03 +0200

    scripts/verify-blacklist: Handle multiple given blacklists
    Also change the message for useless entry to not say blacklist; this
    script is incidentally useful for checking testlist files too.
    
    Cc: Petri Latvala <adrinael@adrinael.net>
    Cc: Mauro Carvalho Chehab <mauro.chehab@linux.intel.com>
    Signed-off-by: Petri Latvala <petri.latvala@intel.com>
    Signed-off-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>
    Reviewed-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- scripts/verify-blacklist.sh
+++ scripts/verify-blacklist.sh
@@ -3,21 +3,23 @@
 # Verify that all entries in a blacklist file are still valid
 
 usage() {
-    echo "Usage: $0 <path-to-igt-runner> <test-binary-directory> <blacklist-file>"
+    echo "Usage: $0 <path-to-igt-runner> <test-binary-directory> <blacklist-files ...>"
     echo
     echo " path-to-igt-runner: For example build/runner/igt_runner"
     echo " test-binary-directory: For example build/tests"
-    echo " blacklist-file: For example tests/intel-ci/blacklist.txt"
+    echo " blacklist-files: For example tests/intel-ci/blacklist.txt"
     exit 2
 }
 
-if [ $# -ne 3 ]; then
+if [ $# -lt 3 ]; then
     usage
 fi
 
 RUNNER="$1"
-BINDIR="$2"
-BLFILE="$3"
+shift
+BINDIR="$1"
+shift
+BLFILES="$*"
 
 if [ ! -x "$RUNNER" ]; then
     echo "$RUNNER not found"
@@ -31,24 +33,28 @@ if [ ! -f "$BINDIR/test-list.txt" ]; then
     usage
 fi
 
-if [ ! -f "$BLFILE" ]; then
-    echo "$BLFILE not found"
-    echo
-    usage
-fi
+for BLFILE in $BLFILES; do
+    if [ ! -f "$BLFILE" ]; then
+	echo "$BLFILE not found"
+	echo
+	usage
+    fi
+done
 
 STATUS=0
 
 TESTLIST="$("$RUNNER" --list-all "$BINDIR")"
 
-cat "$BLFILE" | while read line; do
-    blentry=$(echo "$line" | sed 's/#.*//' | tr -d '[:space:]')
-    if [ "$blentry" = "" ]; then continue; fi
+for BLFILE in $BLFILES; do
+    cat "$BLFILE" | while read line; do
+	blentry=$(echo "$line" | sed 's/#.*//' | tr -d '[:space:]')
+	if [ "$blentry" = "" ]; then continue; fi
 
-    if ! (echo "$TESTLIST" | grep -Pq "$blentry") >/dev/null 2>/dev/null; then
-	echo Useless blacklist entry: "$blentry"
-	STATUS=1
-    fi
+	if ! (echo "$TESTLIST" | grep -Pq "$blentry") >/dev/null 2>/dev/null; then
+	    echo "$BLFILE: Useless entry: $blentry"
+	    STATUS=1
+	fi
+    done
 done
 
 exit $STATUS
